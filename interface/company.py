d45725cd836e4678723554525e7e1fe7b2e3bc5a
BrendanSkyLiang <87059304+BrendanSkyLiang@users.noreply.github.com>
2024-01-28 22:41:18 -0800

    infrastructure changes

--- interface/company.py
+++ interface/company.py
@@ -1,9 +1,9 @@
 import sys
 sys.path.append(r'/Users/brendanliang/Code/brendans_bloomberg')
 import numpy as numpy
-from interface.data_sources.balance_sheet import annual_balance_sheet, balance_sheet_filing, quarterly_balance_sheet
-from interface.data_sources.cashflow_statement import annual_cashflow, quarterly_cashflow, cashflow_filing
-from interface.data_sources.income_statement import annual_income_statement, quarterly_income_statement, income_statement_filing
+from interface.data_sources.balance_sheet import annual_balance_sheet, quarterly_balance_sheet
+from interface.data_sources.cashflow_statement import annual_cashflow, quarterly_cashflow
+from interface.data_sources.income_statement import annual_income_statement, quarterly_income_statement
 from interface.data_sources.price_history import price_history
 from interface.data_sources.profile import profile
 
@@ -11,27 +11,13 @@ class Company():
     def __init__(self, symbol) -> None:
         self.symbol = symbol
         self.profile = profile(symbol=self.symbol)
-        
-    def get_price_history(self) -> price_history:
-        return price_history(self.symbol)
-        
-    def get_annual_balance_sheet(self, year: int) -> balance_sheet_filing:
-        return annual_balance_sheet(self.symbol, year=year).balance_sheet_filing
-    
-    def get_quarterly_balance_sheet(self, year:int, quarter: str) -> balance_sheet_filing:
-        return quarterly_balance_sheet(self.symbol, year=year, quarter=quarter).balance_sheet_filing
-    
-    def get_annual_cashflow(self, year:int) -> cashflow_filing: 
-        return annual_cashflow(self.symbol, year=year).cashflow
-    
-    def get_quarterly_cashflow(self, year:int, quarter:str)->cashflow_filing:
-        return quarterly_cashflow(self.symbol, year=year, quarter=quarter).cashflow_filing
-
-    def get_annual_income_statement(self, year: int) -> income_statement_filing:
-        return annual_income_statement(self.symbol, year=year).income_statement_filing
-    
-    def get_quarterly_income_statement(self, year:int, quarter: str) -> income_statement_filing:
-        return quarterly_income_statement(self.symbol, year=year, quarter=quarter).income_statement_filing
+        self.price_history = price_history(self.symbol)
+        self.annual_balance_sheet = annual_balance_sheet(symbol)
+        self.quarterly_balance_sheet = quarterly_balance_sheet(symbol)
+        self.annual_cashflow = annual_cashflow(symbol)
+        self.quarterly_cashflow = quarterly_cashflow(symbol)
+        self.annual_income_statement = annual_income_statement(symbol)
+        self.quarterly_income_statement = quarterly_income_statement(symbol)
 
 
 
